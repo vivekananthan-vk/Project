91b2b818b598de21adf4b14967e4bff802c1403d
vivek1123devops <vivekananathan.st@gmail.com>
2024-07-08 22:07:52 +0530

    my maven project

---
+++ relaxo/relaxo/src/main/java/com/example/relaxo/RelaxoApplication.java
@@ -0,0 +1,13 @@
+package com.example.relaxo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelaxoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelaxoApplication.class, args);
+	}
+
+}
---
+++ relaxo/relaxo/src/test/java/com/example/relaxo/RelaxoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.relaxo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelaxoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
